e66345f91c1933918dfcea30b2308641ea7f3da3
Romario Teixeira <romario_drt@hotmail.com>
2022-10-06 14:33:49 +0100

    Created inserts for genre database table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- Load Customers 5
+-- Insert Customers 5
 INSERT into customer (creation_date, update_date, email,firstname,lastname,password,is_admin,version)
 VALUES ( now(),now(),'romario@hotmail.com','romario','Teixeira','password',true, 0);
 INSERT into customer (creation_date, update_date, email,firstname,lastname,password,is_admin,version)
@@ -9,3 +9,15 @@ INSERT into customer (creation_date, update_date, email,firstname,lastname,passw
 VALUES ( now(),now(),'Hode@hotmail.com','hode','Kyle','password',false, 0);
 INSERT into customer (creation_date, update_date, email,firstname,lastname,password,is_admin,version)
 VALUES ( now(),now(),'Loke@hotmail.com','Loke','Woods','password',false, 0);
+
+-- Insert genres (10)
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'ADVENTURE', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'MYSTERY', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'FANTASY', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'HORROR', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'ROMANCE', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'NOVEL', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'SUSPENSE', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'DRAMA', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'HISTORY', 0);
+INSERT INTO genre (creation_date,update_date,genre,version) VALUES (now(),now(),'POETRY', 0);
